0051b7cae0cc727186ba3623ec2ac5650e9000d9
Sun04082 <duchai0408@gmail.com>
2022-07-18 16:20:57 +0700

    fm_gioithieu
    Update hinh anh

--- app/src/main/java/com/example/nhatnghia_app/Fragment/Fm_GioiThieu.java
+++ app/src/main/java/com/example/nhatnghia_app/Fragment/Fm_GioiThieu.java
@@ -1,5 +1,6 @@
 package com.example.nhatnghia_app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.nhatnghia_app.R;
 
@@ -16,7 +18,7 @@ import com.example.nhatnghia_app.R;
  * create an instance of this fragment.
  */
 public class Fm_GioiThieu extends Fragment {
-
+    Button btndiachi;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -45,6 +47,7 @@ public class Fm_GioiThieu extends Fragment {
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -61,6 +64,18 @@ public class Fm_GioiThieu extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fm__gioi_thieu, container, false);
+
+
+                View view = inflater.inflate(R.layout.fragment_fm__gioi_thieu, container, false);
+        btndiachi = view.findViewById(R.id.btndiachi);
+        btndiachi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),Fm_DiaChi.class);
+                startActivity(intent);
+            }
+        });
+        return view;
+
     }
 }

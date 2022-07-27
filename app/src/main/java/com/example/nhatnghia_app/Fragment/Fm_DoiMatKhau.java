afdb883ad97c76546ff60a924cfdec7c5da3f0de
Dongocthach <58514860+Dongocthach@users.noreply.github.com>
2022-07-27 16:51:44 +0700

    Revert "Merge branch 'master' of https://github.com/Dongocthach/NhatNghiaApp_1"
    This reverts commit a9bc2e3fa497e7eaada5b841ed17e71ba4d3df8c, reversing
    changes made to 569a414bd24b754355d7f501de45a990e379bb48.

--- app/src/main/java/com/example/nhatnghia_app/Fragment/Fm_DoiMatKhau.java
+++ app/src/main/java/com/example/nhatnghia_app/Fragment/Fm_DoiMatKhau.java
@@ -1,77 +1,66 @@
 package com.example.nhatnghia_app.Fragment;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.example.nhatnghia_app.R;
-import com.example.nhatnghia_app.SignInActivity;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Fm_DoiMatKhau#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class Fm_DoiMatKhau extends Fragment {
 
-    private EditText ed1,ed2;
-    private Button btn;
-    private ProgressDialog progressDialog;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Fm_DoiMatKhau() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Fm_DoiMatKhau.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Fm_DoiMatKhau newInstance(String param1, String param2) {
+        Fm_DoiMatKhau fragment = new Fm_DoiMatKhau();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_fm__doi_mat_khau, container, false);
-        ed1 = v.findViewById(R.id.changepassword_edit_1);
-        ed2 = v.findViewById(R.id.changepassword_edit_2);
-
-        btn = v.findViewById(R.id.changepassword_btn);
-        progressDialog = new ProgressDialog(getActivity());
-
-        btn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                if(ed1.getText().toString().equals(ed2.getText().toString())){
-                    String newPassword = ed1.getText().toString();
-                    progressDialog.show();
-                    user.updatePassword(newPassword)
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isSuccessful()) {
-                                        Toast.makeText(getActivity(),"cap nhap thanh cong",Toast.LENGTH_SHORT).show();
-                                        ed1.setText("");
-                                        ed2.setText("");
-                                        progressDialog.dismiss();
-                                    }
-                                }
-                            });
-
-                }else{
-                    Toast.makeText(getActivity(),"nhap khong giong",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-
-            }
-        });
-        return v;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fm__doi_mat_khau, container, false);
     }
 }
